b25b56960314e8f00475e47fb61090d48c006e35
Rifki Nugraha <rifkichocobo@gmail.com>
2023-09-12 22:33:44 +0800

    test : delete unit test for refactor later

--- src/components/Fragments/__test__/FormBoxProfile.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import FormBoxProfile from '../FormBoxProfile'; 
-
-  test('box tampil dengan benar', () => {
-    render(<FormBoxProfile />);
-
-    expect(screen.getByText('Profile')).toBeInTheDocument();
-  });
--- src/components/Fragments/__test__/FormGameHistory.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import FormGameHistory from '../FormGameHistory';
-
-  test('render judul form game history', () => {
-    render(<FormGameHistory />);
-
-    expect(screen.getByText('Game History')).toBeInTheDocument();
-  });
