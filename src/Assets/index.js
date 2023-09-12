d0e9600732a8eab59d40de609f93d71d51095095
Rifki Nugraha <rifkichocobo@gmail.com>
2023-09-12 22:05:01 +0800

    fix : change assets to src folder

---
+++ src/Assets/index.js
@@ -0,0 +1,71 @@
+import EmailIcon from './email-icon.png';
+import UsernameIcon from './username-icon.png';
+import PasswordIcon from './password-icon.png';
+import ScissorPic from './gunting.png';
+import PaperPic from './kertas.png';
+import RockPic from './batu.png';
+import YellowBtnTop from './home-btn-top.png';
+import YellowBtnSha from './home-btn-shadow.png';
+import GreenBtnTop from './play-btn-top.png';
+import GreenBtnSha from './play-btn-shadow.png';
+import BoxRoomName from './box-room-name.png';
+import VersusAlert from './vs-alert.png';
+import WinNotif from './win-notif.png';
+import LoseNotif from './lose-notif.png';
+import DrawNotif from './draw-notif.png';
+import BigBox from './bg-box.png';
+import RectangleHorz from './bg-box-profile -hori.png';
+import RectangleVert from './bg-box-profile-vert.png';
+import SmallLogo from './small-logo.png';
+import ProfileBoard from './profile-board.png';
+import Avatar from './avatar.png';
+import BgPageNotFound from './404.png';
+import MainBg from './main-bg.jpg';
+import IconShadow from './sidebar-shadow-icon.png';
+import DashboardIcon from './back-button.png';
+import CreateRoomIcon from './createroom-icon.png';
+import GameHistIcon from './gamehist-icon.png';
+import HomeIcon from './home-button.png';
+import LoginIcon from './login-icon.png';
+import LogoutIcon from './logout-button.png';
+import RegisterIcon from './register-icon.png';
+import UserProfileIcon from './profile-button.png';
+import BgSidebarIcon from './bg-sidebar-icon.png';
+import Ribbon from './ribbon.png';
+
+export {
+  EmailIcon,
+  UsernameIcon,
+  PasswordIcon,
+  ScissorPic,
+  PaperPic,
+  RockPic,
+  YellowBtnTop,
+  YellowBtnSha,
+  GreenBtnTop,
+  GreenBtnSha,
+  BoxRoomName,
+  VersusAlert,
+  WinNotif,
+  LoseNotif,
+  DrawNotif,
+  BigBox,
+  RectangleHorz,
+  RectangleVert,
+  SmallLogo,
+  ProfileBoard,
+  Avatar,
+  BgPageNotFound,
+  MainBg,
+  IconShadow,
+  DashboardIcon,
+  CreateRoomIcon,
+  GameHistIcon,
+  HomeIcon,
+  LoginIcon,
+  LogoutIcon,
+  RegisterIcon,
+  UserProfileIcon,
+  BgSidebarIcon,
+  Ribbon,
+};
